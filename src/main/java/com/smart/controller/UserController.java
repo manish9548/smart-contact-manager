a059ccbeae8005db9be2b78bf03f05abe8a01c8c
manish9548 <baghelmanish4947@gmail.com>
2026-02-16 23:31:57 +0530

    Added add contact form, dashboard and controller updates

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -5,9 +5,12 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smart.dao.UserRepository;
+import com.smart.entities.Contact;
 import com.smart.entities.User;
 
 @Controller
@@ -15,16 +18,31 @@ import com.smart.entities.User;
 public class UserController {
 	@Autowired
 	private UserRepository userRepository;
-	
-	@RequestMapping("/index")
-	public String dashboard(Model model ,Principal principal)
-	{
+	//methode for adding common data to response 
+	@ModelAttribute
+	public void addCommonData(Model model,Principal principal) {
 		String userName=principal.getName();
 		System.out.println("USERNAME" + userName);
 		User user =userRepository.findByEmail(userName);
 		System.out.println("User Name: " + user.getName());
 		model.addAttribute("user", user);
+		
+	}
+	
+	@RequestMapping("/index")
+	public String dashboard(Model model ,Principal principal)
+	{
+		model.addAttribute("title", "User Dashboard");
 		return "normal/user_dashboard";
+	}
+	//open add form handler
+	@GetMapping("/add-contact")
+	public String opeAddContactForm(Model model) {
+		model.addAttribute("title", "Add Contact");
+		model.addAttribute("contact",new Contact());
+		
+		
+		return "normal/add_contact_form";
 	}
 
 }
